cae690fbaaed2d0391839aefbabc1327e750aab1
Shay Maor <shay.h.maor@gmail.com>
2016-11-24 21:47:17 -0500

    Past session crawl location and logic fix up

--- SNPedia/SNPGen.py
+++ SNPedia/SNPGen.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 
 class GrabSNPs:
@@ -6,27 +7,30 @@ class GrabSNPs:
     crawls and attains a list of SNPedia compatible SNPs found within the snps of interest array
     """
 
-    def __init__(self, crawllimit=300, snpsofinterest=None, target=50):
+    def __init__(self, crawllimit=500, snpsofinterest=None, target=50):
         self.cmcontinue = ""
-        print(snpsofinterest[:50])
         self.snps = []
         self.limit = crawllimit
         self.target = target
-        if snpsofinterest:
-            self.crawl(snpsofinterest=snpsofinterest, target=target)
+        if self.lastsessionexists():
+            cmcontinue = self.importlast()
         else:
-            self.crawl(target=target)
+            cmcontinue = None
+
+        if snpsofinterest:
+            self.crawl(snpsofinterest=snpsofinterest, cmcontinue=cmcontinue, target=target)
+        else:
+            self.crawl(target=target, cmcontinue=cmcontinue)
+        self.export()
+
 
     def crawl(self, snpsofinterest=None, cmcontinue=None, target=100):
         members = []
-        print(cmcontinue)
         count = 0
         if not cmcontinue:
             curgen = "http://bots.snpedia.com//api.php?action=query&format=json&list=categorymembers&rawcontinue=0&cmtitle=Category%3Ais_a_snp"
             response = requests.get(curgen)
             jd = response.json()
-            
-            print(jd)
 
             members.append(jd["query"]["categorymembers"])
             cmcontinue = jd["query-continue"]["categorymembers"]["cmcontinue"]
@@ -36,19 +40,14 @@ class GrabSNPs:
                      + cmcontinue
             response = requests.get(curgen)
             jd = response.json()
-            #print(jd)
             members.append(jd["query"]["categorymembers"])
             cmcontinue = jd["query-continue"]["categorymembers"]["cmcontinue"]
             count += 1
 
         members = [[i["title"] for i in item] for item in members]
 
-
         for member in members:
             self.snps.extend(member)
-            
-
-
 
         if snpsofinterest:
             self.snps = [snp for snp in self.snps if snp.lower() in snpsofinterest]
@@ -58,6 +57,27 @@ class GrabSNPs:
             self.crawl(snpsofinterest=snpsofinterest, cmcontinue=cmcontinue, target=target)
 
         self.cmcontinue = cmcontinue
+
+    def lastsessionexists(self):
+        filepath = os.path.join(os.path.curdir, "data", 'last_session.txt')
+        return os.path.isfile(filepath)
+
+    def importlast(self):
+        filepath = os.path.join(os.path.curdir, "data", 'last_session.txt')
+        lastsession = open(filepath, "r")
+        lines = lastsession.readlines()
+        lastsession.close()
+        lastsessionvalue = lines[0].strip("\n")
+        print(lastsessionvalue)
+        return lastsessionvalue
+
+
+    def export(self):
+        filepath = os.path.join(os.path.curdir, "data", 'last_session.txt')
+        with open(filepath, "w") as lastsession:
+            lastsession.write(self.cmcontinue)
+            lastsession.close()
+
 
 
 
