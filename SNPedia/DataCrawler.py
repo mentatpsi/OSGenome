5ed5a7cdcfc6c7ef9fded545dd45308c3e38c4dd
Shay Maor <shay.h.maor@gmail.com>
2018-02-14 17:01:45 -0500

    Fix of Directory Not found

--- SNPedia/DataCrawler.py
+++ SNPedia/DataCrawler.py
@@ -134,6 +134,8 @@ args = vars(parser.parse_args())
 rsid = ["rs1815739", "Rs53576", "rs4680", "rs1800497", "rs429358", "rs9939609", "rs4988235", "rs6806903", "rs4244285"]
 rsid += ["rs1801133"]
 
+os.chdir(os.path.dirname(__file__))
+
 
 if args["filepath"]:
     personal = PersonalData(args["filepath"])
